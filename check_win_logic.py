252027153b5b101b872237d7402116c179dbd1f2
lk2147 <2kkpv2@gmail.com>
2023-12-01 22:35:02 +0700

    check 5

--- check_win_logic.py
+++ check_win_logic.py
@@ -12,8 +12,8 @@ def check_win(num_row, num_col, cur_x, cur_y, cnt_p, turn):
         pre_len = 0
         if inside(num_row, num_col, pre_x, pre_y) and (cnt_p[pre_x][pre_y][i][turn] > 0):
             pre_len = cnt_p[pre_x][pre_y][i][turn]
-            pre_x = cur_x + pre_len * dir_x[i]
-            pre_y = cur_y + pre_len * dir_y[i]
+            pre_x = cur_x + (pre_len - 1) * dir_x[i]
+            pre_y = cur_y + (pre_len - 1) * dir_y[i]
         else:
             pre_x = cur_x
             pre_y = cur_y
@@ -23,8 +23,8 @@ def check_win(num_row, num_col, cur_x, cur_y, cnt_p, turn):
         nxt_len = 0
         if inside(num_row, num_col, nxt_x, nxt_y) and (cnt_p[nxt_x][nxt_y][7 - i][turn] > 0):
             nxt_len = cnt_p[nxt_x][nxt_y][7 - i][turn]
-            nxt_x = cur_x - nxt_len * dir_x[i]
-            nxt_y = cur_y - nxt_len * dir_y[i]
+            nxt_x = cur_x - (nxt_len - 1) * dir_x[i]
+            nxt_y = cur_y - (nxt_len - 1) * dir_y[i]
         else:
             nxt_x = cur_x
             nxt_y = cur_y
