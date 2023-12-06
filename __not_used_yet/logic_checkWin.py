6bd7cbeb395b76dbf69726b913b5dfce49fc57ac
lk2147 <2kkpv2@gmail.com>
2023-12-06 13:44:40 +0700

    them check_win vao not_used

--- __not_used_yet/logic_checkWin.py
+++ __not_used_yet/logic_checkWin.py
@@ -1,32 +1,45 @@
-import numpy
+dir_x = (1, 0, -1, -1)
+dir_y = (-1, -1, -1, 0)
 
-dir_x = numpy.array((-1, -1, 0, 1))
-dir_y = numpy.array((0, -1, -1, 1))
+def inside(num_row, num_col, cur_x, cur_y):
+    return (0 <= cur_x < num_row) and (0 <= cur_y < num_col)
 
-def inside(numRow, numCol, x, y):
-    return (0 <= x <= numRow) and (0 <= y <= numCol)
-    
-def checkWin(numRow, numCol, cur_row, cur_col, cnt):
-    ok = False
-    for t in range(4):
-        both = 1
-
-        pre_x = cur_row + dir_x[t]
-        pre_y = cur_col + dir_y[t]
-        if inside(numRow, numCol, pre_x, pre_y):
-            both += cnt[pre_x][pre_y] 
-            cnt[cur_row][cur_col][t] = cnt[pre_x][pre_y][7 - t] + 1
+def check_win(num_row, num_col, cur_x, cur_y, cnt_p, turn):
+    is_win = False
+    for i in range(4):
+        pre_x = cur_x + dir_x[i]
+        pre_y = cur_y + dir_y[i]
+        pre_len = 0
+        if inside(num_row, num_col, pre_x, pre_y) and (cnt_p[pre_x][pre_y][i][turn] > 0):
+            pre_len = cnt_p[pre_x][pre_y][i][turn]
+            pre_x = cur_x + pre_len * dir_x[i]
+            pre_y = cur_y + pre_len * dir_y[i]
         else:
-            cnt[cur_row][cur_col][t] = 1
+            pre_x = cur_x
+            pre_y = cur_y
 
-        nxt_x = cur_row - dir_x[t]
-        nxt_y = cur_row - dir_y[t]
-        if inside(numRow, numCol, nxt_x, nxt_y):
-            both += cnt[nxt_x][nxt_y]
-            cnt[cur_row][cur_col][7 - t] = cnt[nxt_x][nxt_y][t]  + 1
+        nxt_x = cur_x - dir_x[i]
+        nxt_y = cur_y - dir_y[i]
+        nxt_len = 0
+        if inside(num_row, num_col, nxt_x, nxt_y) and (cnt_p[nxt_x][nxt_y][7 - i][turn] > 0):
+            nxt_len = cnt_p[nxt_x][nxt_y][7 - i][turn]
+            nxt_x = cur_x - nxt_len * dir_x[i]
+            nxt_y = cur_y - nxt_len * dir_y[i]
         else:
-            cnt[cur_row][cur_col][7 - t] = 1
+            nxt_x = cur_x
+            nxt_y = cur_y
+        
+        cnt_p[pre_x][pre_y][7 - i][turn] = cnt_p[nxt_x][nxt_y][i][turn] = pre_len + 1 + nxt_len
+        if (pre_len + nxt_len > 3):
+            pre_x += dir_x[i]
+            pre_y += dir_y[i]
+            pre_blocked = inside(num_row, num_col, pre_x, pre_y) and (cnt_p[pre_x][pre_y][i][1 - turn] > 0)
 
-        ok |= both >= 5
+            nxt_x -= dir_x[i]
+            nxt_y -= dir_y[i]
+            nxt_blocked = inside(num_row, num_col, nxt_x, nxt_y) and (cnt_p[nxt_x][nxt_y][7 - i][1 - turn] > 0)
+            
+            if (pre_blocked & nxt_blocked) == False:
+                is_win = True
 
-    return ok
+    return is_win
