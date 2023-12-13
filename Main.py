bf8cd243caccffbdb44daa9db301063a193a755c
lk2147 <2kkpv2@gmail.com>
2023-12-13 22:27:44 +0700

    chinh lai screen size chu to qua=))

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import pygame, Intro, Define
 pygame.init()
 pygame.mixer.init()
 
-SCREEN = pygame.display.set_mode((1280, 720))
+SCREEN = pygame.display.set_mode((1273, 715))
 pygame.display.set_caption("Menu")
 
 Define.Sound.BACKGROUND_MUSIC.play(-1)
